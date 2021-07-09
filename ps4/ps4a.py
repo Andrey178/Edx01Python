6d937f1b7d93803455de6f6633883837460f7ffe
Andre178 <santa-andrio@mail.ru>
2021-07-09 19:40:37 +0300

    Finish Problem Set 4, Problem 6 Realize playGame function

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -257,8 +257,10 @@ def playHand(hand, wordList, n):
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
     if user_word == ".":
         print("Goodbye! Total score: %s points." % total_score)
+        print()
     else:
         print("Run out of letters. Total score: %s points." % total_score)
+        print()
 
 #
 # Problem #5: Playing a game
@@ -277,7 +279,29 @@ def playGame(wordList):
     2) When done playing the hand, repeat from step 1    
     """
     # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+#    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    hand = ''
+
+    while True:
+        game_status = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if game_status == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+            print()
+        elif game_status == 'r':
+            if len(hand) == 0:
+                print("You have not played a hand yet. Please play a new hand first!")
+                print()
+                continue
+            else:
+                playHand(hand, wordList, HAND_SIZE)
+        elif game_status == 'e':
+            break
+        else:
+            print('Invalid command.')
+            print()
+            continue
+
    
 
 
